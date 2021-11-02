995b8ab7e50e66cab6dfbc4c7dc9a14f1754daa5
dscyrescotti <dscyrescotti@gmail.com>
2021-11-02 13:21:48 +0630

    feat: add example project

---
+++ Example/Shared/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Shared
+//
+//  Created by Dscyre Scotti on 02/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
