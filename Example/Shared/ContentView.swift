ed9be197b90b1841fd3699175c6528a16a3a8567
dscyrescotti <dscyrescotti@gmail.com>
2021-11-06 21:45:22 +0630

    feat: implement showing dialog alert

--- Example/Shared/ContentView.swift
+++ Example/Shared/ContentView.swift
@@ -6,11 +6,73 @@
 //
 
 import SwiftUI
+import SwiftTheming
 
 struct ContentView: View {
+    @State private var isShowing: Bool = false
+    @ScaledMetric(relativeTo: .title3) var imageSize: CGFloat = 20
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Color.clear
+                .overlay(alignment: .bottomTrailing, content: {
+                    Button(action: {
+                        isShowing.toggle()
+                    }) {
+                        Color.white
+                            .frame(width: 30, height: 30)
+                            .mask(
+                                Image("palette")
+                                    .resizable()
+                            )
+                            .padding()
+                            .background(Color.blue)
+                            .clipShape(Circle())
+                    }
+                    .padding()
+                })
+                .navigationTitle("SwiftTheming")
+        }
+        .overlay {
+            if isShowing {
+                Color.clear
+                    .background(.ultraThinMaterial)
+                    .transition(.opacity.animation(.spring()))
+                    .onTapGesture {
+                        isShowing.toggle()
+                    }
+            }
+        }
+        .overlay {
+            if isShowing {
+                VStack {
+                    HStack {
+                        Label(title: {
+                            Text("Color Themes")
+                                .font(.title3.bold())
+                        }, icon: {
+                            Image("palette")
+                                .resizable()
+                                .frame(width: imageSize, height: imageSize)
+                        })
+                        Spacer()
+                        Button(action: {
+                            isShowing.toggle()
+                        }) {
+                            Image(systemName: "xmark")
+                                .font(.headline.bold())
+                        }
+                        .foregroundColor(.primary)
+                    }
+                    Spacer()
+                }
+                .padding()
+                .frame(maxWidth: 400, maxHeight: 500)
+                .background(.background)
+                .cornerRadius(20)
+                .padding(.all, 30)
+                .transition(AnyTransition.move(edge: .bottom).animation(.default))
+            }
+        }
     }
 }
 
@@ -19,3 +81,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- Example/Shared/MainApp.swift
+++ Example/Shared/MainApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftTheming
 
 @main
 struct MainApp: App {
