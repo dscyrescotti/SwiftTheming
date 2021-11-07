14d5cfd9fe45eac8f3a2193dff1c749d07cf5d03
dscyrescotti <dscyrescotti@gmail.com>
2021-11-07 15:51:31 +0630

    feat: mark open keyword instead of public

--- Sources/SwiftTheming/Themed.swift
+++ Sources/SwiftTheming/Themed.swift
@@ -1,19 +1,19 @@
 import SwiftUI
 
 public class Themed<Asset: Assetable> {
-    public func colorSet(for asset: Asset.ColorAsset) -> ColorSet {
+    open func colorSet(for asset: Asset.ColorAsset) -> ColorSet {
         fatalError("Need to override the \(#function) method.")
     }
     
-    public func imageSet(for asset: Asset.ImageAsset) -> ImageSet {
+    open func imageSet(for asset: Asset.ImageAsset) -> ImageSet {
         fatalError("Need to override the \(#function) method.")
     }
     
-    public func gradientSet(for asset: Asset.GradientAsset) -> GradientSet {
+    open func gradientSet(for asset: Asset.GradientAsset) -> GradientSet {
         fatalError("Need to override the \(#function) method.")
     }
     
-    public func fontSet(for asset: Asset.FontAsset) -> FontSet {
+    open func fontSet(for asset: Asset.FontAsset) -> FontSet {
         fatalError("Need to override the \(#function) method.")
     }
 }
