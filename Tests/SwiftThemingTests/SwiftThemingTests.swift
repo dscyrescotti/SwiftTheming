3c54412baa7248180df0143d18b8116003f774ee
dscyrescotti <dscyrescotti@gmail.com>
2021-11-28 23:44:31 +0630

    test: write test case for theme provider

--- Tests/SwiftThemingTests/SwiftThemingTests.swift
+++ Tests/SwiftThemingTests/SwiftThemingTests.swift
@@ -5,17 +5,39 @@ import ViewInspector
 @testable import SwiftTheming
 
 final class SwiftThemingTests: XCTestCase {
-    func testThemeProvider() {
+    
+    func testThemeProviderWithSpy() {
         let themeProvider = ThemeProvider<Theme>(defaultTheme: .bluoTheme, preferredAppearance: .light)
         themeProvider.setTheme(with: .bluoTheme)
         themeProvider.setPreferredAppearance(with: .light)
-        XCTAssertEqual(themeProvider.theme, .bluoTheme)
+        
+        let themeSpy = ValueSpy<Theme>.init(themeProvider.$theme.eraseToAnyPublisher())
+        themeProvider.setTheme(with: .bluoTheme)
         themeProvider.setTheme(with: .jadoTheme)
-        XCTAssertEqual(themeProvider.theme, .jadoTheme)
-        XCTAssertEqual(themeProvider.preferredAppearance, .light)
+        themeProvider.setTheme(with: .jadoTheme)
+        XCTAssertEqual(themeSpy.values, [.bluoTheme, .jadoTheme])
+        
+        let appearanceSpy = ValueSpy<PreferredAppearance>.init(themeProvider.$preferredAppearance.eraseToAnyPublisher())
         themeProvider.setPreferredAppearance(with: .dark)
-        XCTAssertEqual(themeProvider.preferredAppearance, .dark)
+        themeProvider.setPreferredAppearance(with: .dark)
+        themeProvider.setPreferredAppearance(with: .light)
         themeProvider.setPreferredAppearance(with: .system)
-        XCTAssertEqual(themeProvider.preferredAppearance, .system)
+        XCTAssertEqual(appearanceSpy.values, [.light, .dark, .light, .system])
+        
+        let voidSpy = ValueSpy<Void>.init(themeProvider.objectWillChange.eraseToAnyPublisher())
+        themeProvider.setPreferredAppearance(with: .light)
+        themeProvider.setTheme(with: .bluoTheme)
+        themeProvider.changeColorScheme(with: .light)
+        themeProvider.setPreferredAppearance(with: .system)
+        themeProvider.setPreferredAppearance(with: .system)
+        XCTAssertEqual(voidSpy.values.count, 4)
+        
+        let colorSchemeSpy = ValueSpy<ColorScheme?>.init(themeProvider.$colorScheme.eraseToAnyPublisher())
+        themeProvider.changeColorScheme(with: .light)
+        themeProvider.changeColorScheme(with: .dark)
+        themeProvider.changeColorScheme(with: .dark)
+        themeProvider.changeColorScheme(with: .light)
+        XCTAssertEqual(colorSchemeSpy.values, [.light, .dark, .light])
     }
+    
 }
