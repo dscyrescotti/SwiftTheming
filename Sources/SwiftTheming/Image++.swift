af96a8782cd44e1fc5f630a317f3f55a947fd7d9
Dscyre Scotti <dscyrescotti@Dscyres-MacBook-Pro.local>
2021-11-28 14:39:52 +0630

    feat: add image initializers

---
+++ Sources/SwiftTheming/Image++.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+public extension Image {
+    /// An image of a given asset.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset) {
+        self = themeProvider.image(for: asset, preferredAppearance: nil, on: nil)
+    }
+    
+    /// An image of a given asset that allows to override the preferred appearance.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance) {
+        self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: nil)
+    }
+    
+    /// An image of a given asset that allows to override the preferred appearance.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance?) {
+        self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: nil)
+    }
+    
+    /// An image of a given asset that allows to override the current theme optionally.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, theme: Theme?) {
+        self = themeProvider.image(for: asset, preferredAppearance: nil, on: theme)
+    }
+    
+    /// An image of a given asset that allows to override the current theme.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, theme: Theme) {
+        self = themeProvider.image(for: asset, preferredAppearance: nil, on: theme)
+    }
+    
+    /// An image of a given asset that allows to override the preferred appearance and the current theme.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance, theme: Theme) {
+        self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: theme)
+    }
+    
+    /// An image of a given asset that allows to override the preferred appearance and the current theme optionally.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance?, theme: Theme?) {
+        self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: theme)
+    }
+}
