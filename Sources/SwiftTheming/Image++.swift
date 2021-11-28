bafbf1164555ed6dfe7ce8a5309c4526f20997d8
dscyrescotti <dscyrescotti@gmail.com>
2021-11-28 15:07:18 +0630

    feat: remove some initializers

--- Sources/SwiftTheming/Image++.swift
+++ Sources/SwiftTheming/Image++.swift
@@ -7,11 +7,6 @@ public extension Image {
     }
     
     /// An image of a given asset that allows to override the preferred appearance.
-    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance) {
-        self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: nil)
-    }
-    
-    /// An image of a given asset that allows to override the preferred appearance.
     init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance?) {
         self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: nil)
     }
@@ -19,16 +14,6 @@ public extension Image {
     /// An image of a given asset that allows to override the current theme optionally.
     init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, theme: Theme?) {
         self = themeProvider.image(for: asset, preferredAppearance: nil, on: theme)
-    }
-    
-    /// An image of a given asset that allows to override the current theme.
-    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, theme: Theme) {
-        self = themeProvider.image(for: asset, preferredAppearance: nil, on: theme)
-    }
-    
-    /// An image of a given asset that allows to override the preferred appearance and the current theme.
-    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.ImageAsset, preferredAppearance: PreferredAppearance, theme: Theme) {
-        self = themeProvider.image(for: asset, preferredAppearance: preferredAppearance, on: theme)
     }
     
     /// An image of a given asset that allows to override the preferred appearance and the current theme optionally.
