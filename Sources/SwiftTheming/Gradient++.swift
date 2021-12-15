beea2db1d76f0f1f1947ebda535a2212a27ea14d
ayechan <ayechan@zig.live>
2021-12-15 13:41:17 +0630

    feat: add radial gradient initializers

--- Sources/SwiftTheming/Gradient++.swift
+++ Sources/SwiftTheming/Gradient++.swift
@@ -44,3 +44,26 @@ extension LinearGradient {
         self.init(gradient: Gradient(on: themeProvider, for: asset, preferredAppearance: preferredAppearance, theme: theme), startPoint: startPoint, endPoint: endPoint)
     }
 }
+
+// MARK: - RadialGradient
+extension RadialGradient {
+    /// A radial gradient of a given asset.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.GradientAsset, center: UnitPoint, startRadius: CGFloat, endRadius: CGFloat) {
+        self.init(gradient: Gradient(on: themeProvider, for: asset), center: center, startRadius: startRadius, endRadius: endRadius)
+    }
+    
+    /// A radial gradient of a given asset that allows to override the preferred appearance.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.GradientAsset, preferredAppearance: PreferredAppearance?, center: UnitPoint, startRadius: CGFloat, endRadius: CGFloat) {
+        self.init(gradient: Gradient(on: themeProvider, for: asset, preferredAppearance: preferredAppearance), center: center, startRadius: startRadius, endRadius: endRadius)
+    }
+    
+    /// A radial gradient of a given asset that allows to override the current theme optionally.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.GradientAsset, theme: Theme?, center: UnitPoint, startRadius: CGFloat, endRadius: CGFloat) {
+        self.init(gradient: Gradient(on: themeProvider, for: asset, theme: theme), center: center, startRadius: startRadius, endRadius: endRadius)
+    }
+    
+    /// A radial gradient of a given asset that allows to override the preferred appearance and the current theme optionally.
+    init<Theme: Themeable>(on themeProvider: ThemeProvider<Theme>, for asset: Theme.ThemeAsset.GradientAsset, preferredAppearance: PreferredAppearance?, theme: Theme?, center: UnitPoint, startRadius: CGFloat, endRadius: CGFloat) {
+        self.init(gradient: Gradient(on: themeProvider, for: asset, preferredAppearance: preferredAppearance, theme: theme), center: center, startRadius: startRadius, endRadius: endRadius)
+    }
+}
