449f94b49ffdaa3ee0f51c41558e3c5269f79628
ayechan <ayechan@zig.live>
2022-03-20 19:14:13 +0630

    feat: mark public keyword and add public init

--- Sources/SwiftTheming/ThemeProviding.swift
+++ Sources/SwiftTheming/ThemeProviding.swift
@@ -1,10 +1,12 @@
 import SwiftUI
 
 @propertyWrapper
-struct ThemeProviding<Theme: Themeable>: DynamicProperty {
+public struct ThemeProviding<Theme: Themeable>: DynamicProperty {
     @EnvironmentObject private var themeProvider: ThemeProvider<Theme>
     
     public var wrappedValue: ThemeProvider<Theme> {
         themeProvider
     }
+    
+    public init() { }
 }
