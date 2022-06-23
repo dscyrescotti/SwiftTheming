cc1e42da1f5d64b671b1a6d7fbc883927936675f
dscyrescotti <dscyrescotti@gmail.com>
2022-06-23 12:35:37 +0630

    feat: add cocoapod support

--- Sources/SwiftTheming/Appearance.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-enum Appearance<T: Equatable>: Equatable {
-    case `static`(T)
-    case dynamic(light: T, dark: T)
-}
--- Sources/SwiftTheming/PreferredAppearance.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-/// A preferred appearance to override system appearance.
-public enum PreferredAppearance: Codable {
-    case system
-    case light
-    case dark
-}
