66f4f30153fc77e89a8ac0c11f9fdc906c5bc2ca
dscyrescotti <dscyrescotti@gmail.com>
2022-08-20 16:00:58 +0630

    feat: implement themed color view

--- Sources/SwiftTheming/Views/ThemedColor.swift
+++ Sources/SwiftTheming/Views/ThemedColor.swift
@@ -8,8 +8,10 @@ public struct ThemedColor: View, ShapeStyle {
     @Environment(\.preferredAppearance) var preferredAppearance
     @Environment(\.preferredTheme) var preferredTheme
     
+    private let _opacity: Double
+    
     public init(_ asset: ColorAssetable) {
-        self.asset = asset
+        self.init(asset: asset)
     }
     
     public static func _makeView<S>(view: SwiftUI._GraphValue<SwiftUI._ShapeView<S, SwiftUI.Color>>, inputs: SwiftUI._ViewInputs) -> SwiftUI._ViewOutputs where S : SwiftUI.Shape {
@@ -17,10 +19,31 @@ public struct ThemedColor: View, ShapeStyle {
     }
 
     public func _apply(to shape: inout SwiftUI._ShapeStyle_Shape) {
-        themeProvider.color(for: asset, preferredAppearance: preferredAppearance, on: preferredTheme)._apply(to: &shape)
+        themeProvider.color(
+            for: asset,
+            preferredAppearance: preferredAppearance,
+            on: preferredTheme
+        )
+        .opacity(_opacity)
+        ._apply(to: &shape)
     }
 
     public static func _apply(to type: inout SwiftUI._ShapeStyle_ShapeType) {
         Color._apply(to: &type)
+    }
+}
+
+@available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
+extension ThemedColor {
+    internal init(
+        asset: ColorAssetable,
+        opacity: Double = 1
+    ) {
+        self.asset = asset
+        self._opacity = opacity
+    }
+    
+    public func opacity(_ opacity: Double) -> ThemedColor {
+        ThemedColor(asset: asset, opacity: opacity)
     }
 }
---
+++ Sources/SwiftTheming/Views/ThemedImage.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Aye Chan on 8/20/22.
+//
+
+import Foundation
